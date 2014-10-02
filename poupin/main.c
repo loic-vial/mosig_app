e979ffca9a1e1ecd3af2eefd89d5b118b898ba8f
Pierre Poupin <pierre.poupin@ensimag.grenoble-inp.fr>
2014-10-02 13:22:29 +0200

    Ajout de la struct (poupin)

--- poupin/main.c
+++ poupin/main.c
@@ -4,7 +4,8 @@
 
 
 int main(int argc, char **argv) {
-  unsigned int len;
+/*   unsigned int len;
+ */
   if (argc < 3) {
     printf("Usage: %s <width> <height>\n", *argv);
     return 1;
--- poupin/maze.h
+++ poupin/maze.h
@@ -1,5 +1,19 @@
 typedef struct _tree tree;
-typedef struct _maze maze;
+
+struct _tree {
+	int width, height;
+	int offset_w, offset_h;
+	int door_position;
+	int wall_position;
+	tree *left_child_bottom;
+	tree *right_child_top;
+	tree *parent;
+	int is_wall_vertical;
+};
+
+typedef struct _maze {
+	tree* room;
+} maze;
 
 maze *maze_random (int width, int height);
 void maze_svg (maze *maze, char *filename);
